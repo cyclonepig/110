38205b03c76f97ae4d26ab03f82fd484f84b5139
nexteyes@outlook.com <nexteyes@outlook.com>
2018-03-30 17:01:40 +0800

    first try

--- src/main/java/com/tongdao/base/commonDao/userInfoDao.java
+++ src/main/java/com/tongdao/base/commonDao/userInfoDao.java
@@ -1,11 +1,10 @@
 package com.tongdao.base.commonDao;
 
 import com.tongdao.base.model.User.Userinfo;
-import com.tongdao.base.model.product.cementGrindingAid.Cementgrindingaid;
 import com.tongdao.core.Dao.dao.Daoimpl.Daoimpl;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class userInfoDao extends Daoimpl<Userinfo,Long> {
+public class userInfoDao extends Daoimpl<Userinfo,Integer> {
 }
